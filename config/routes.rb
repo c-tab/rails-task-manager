782dc81bb44edb189c932585737764a4e099c7ab
Carlos Aguirre <carlostomasaguirre@yahoo.com>
2023-05-23 15:49:36 +0200

    CRUD RAILS EXAMPLE DONE

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'tasks#home'
+  get 'tasks', to: 'tasks#index'
+  get 'new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+  patch 'tasks/:id', to: 'tasks#update'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
